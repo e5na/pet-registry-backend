b6eb2a3d397e374a7c81b315d2d5d3d6c3a17789
Marko Esna <marko.esna@gmail.com>
2026-03-08 10:16:51 +0200

    feat: add core domain entities

---
+++ src/main/java/com/petreg/prototype/model/Microchip.java
@@ -0,0 +1,51 @@
+package com.petreg.prototype.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "microchips")
+public class Microchip {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private int chipNumber;
+
+    private String supplier;
+    private boolean inUse;
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getChipNumber() {
+        return chipNumber;
+    }
+
+    public void setChipNumber(int chipNumber) {
+        this.chipNumber = chipNumber;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
+    public boolean isInUse() {
+        return inUse;
+    }
+
+    public void setInUse(boolean inUse) {
+        this.inUse = inUse;
+    }
+}
---
+++ src/main/java/com/petreg/prototype/model/Pet.java
@@ -0,0 +1,124 @@
+package com.petreg.prototype.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pets")
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private char sex;
+    private LocalDate birthDate;
+    private String color;
+
+    @Lob
+    @Column(name = "image_data", columnDefinition = "BYTEA")
+    private byte[] picture;
+
+    @ManyToOne
+    @JoinColumn(name = "species_id")
+    private Species species;
+
+    @ManyToOne
+    @JoinColumn(name = "breed_id")
+    private Breed breed;
+
+    @OneToOne
+    @JoinColumn(name = "microchip_id", referencedColumnName = "id", unique = true)
+    private Microchip microchip;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public byte[] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(byte[] picture) {
+        this.picture = picture;
+    }
+
+    public Species getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(Species species) {
+        this.species = species;
+    }
+
+    public Breed getBreed() {
+        return breed;
+    }
+
+    public void setBreed(Breed breed) {
+        this.breed = breed;
+    }
+
+    public Microchip getMicrochip() {
+        return microchip;
+    }
+
+    public void setMicrochip(Microchip microchip) {
+        this.microchip = microchip;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+}
