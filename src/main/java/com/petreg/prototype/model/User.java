eb1dfcb2ba3d398e938db8822433f70b5d3e5743
teadatuntud <265739897+teadatuntud@users.noreply.github.com>
2026-03-11 15:21:15 +0200

    style: reformat User model

--- src/main/java/com/petreg/prototype/model/User.java
+++ src/main/java/com/petreg/prototype/model/User.java
@@ -32,7 +32,8 @@ public class User {
     private String address;
 
     @ManyToMany
-    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
     public Long getId() {
