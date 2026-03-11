3095e5a462164ec805b76382a4f6198c3496b89d
teadatuntud <265739897+teadatuntud@users.noreply.github.com>
2026-03-11 15:24:05 +0200

    refactor: add constructors to Role model

--- src/main/java/com/petreg/prototype/model/Role.java
+++ src/main/java/com/petreg/prototype/model/Role.java
@@ -25,6 +25,12 @@ public class Role {
     @ManyToMany(mappedBy = "roles")
     private Set<User> users = new HashSet<>();
 
+    public Role() {}
+
+    public Role(String name) {
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
