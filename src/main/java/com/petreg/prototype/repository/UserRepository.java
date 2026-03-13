6a0fdd3a4d0c5c0d36e232cb86c565794e5d5e97
teadatuntud <265739897+teadatuntud@users.noreply.github.com>
2026-03-13 13:47:52 +0200

    feat: allow to query users by personal code

--- src/main/java/com/petreg/prototype/repository/UserRepository.java
+++ src/main/java/com/petreg/prototype/repository/UserRepository.java
@@ -1,8 +1,14 @@
 package com.petreg.prototype.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.petreg.prototype.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByPersonalCode(String personalCode);
+
+    boolean existsByPersonalCode(String personalCode);
 }
