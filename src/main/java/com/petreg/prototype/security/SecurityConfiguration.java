6c22dce070ff9fe3de08c4d9f9a759cb9d126db8
teadatuntud <265739897+teadatuntud@users.noreply.github.com>
2026-03-16 16:23:44 +0200

    fix: ensure that authentication is stateless

--- src/main/java/com/petreg/prototype/security/SecurityConfiguration.java
+++ src/main/java/com/petreg/prototype/security/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,7 +26,11 @@ public class SecurityConfiguration {
                 .anyRequest().authenticated()
             )
             // Enable Basic Auth
-            .httpBasic(Customizer.withDefaults());
+            .httpBasic(Customizer.withDefaults())
+            // No cookies, please
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            );
 
         return http.build();
     }
