04f15aa3c879907a82662bfe78bf5c727210e890
teadatuntud <265739897+teadatuntud@users.noreply.github.com>
2026-03-20 17:29:17 +0200

    feat: allow users to be queried by role name

--- src/main/java/com/petreg/prototype/repository/UserRepository.java
+++ src/main/java/com/petreg/prototype/repository/UserRepository.java
@@ -1,14 +1,19 @@
 package com.petreg.prototype.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.petreg.prototype.model.User;
+import com.petreg.prototype.model.type.RoleEnum;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByPersonalCode(String personalCode);
 
     boolean existsByPersonalCode(String personalCode);
+
+    List<User> findByRoles_Name(RoleEnum roleName);
+
 }
