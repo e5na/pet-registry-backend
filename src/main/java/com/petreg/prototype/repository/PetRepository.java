afcc685f753ae331efee31a79e96cc8f7f875ee3
teadatuntud <265739897+teadatuntud@users.noreply.github.com>
2026-03-23 10:29:36 +0200

    feat: implement API endpoint to get owner pets
    Query:
    
        GET /api/users/:id/pets

--- src/main/java/com/petreg/prototype/repository/PetRepository.java
+++ src/main/java/com/petreg/prototype/repository/PetRepository.java
@@ -1,7 +1,12 @@
 package com.petreg.prototype.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.petreg.prototype.model.Pet;
 
-public interface PetRepository extends JpaRepository<Pet, Long> {}
+public interface PetRepository extends JpaRepository<Pet, Long> {
+
+    List<Pet> findByOwnerId(Long ownerId);
+}
