6664b2428fad8c47eab52ac2c1745ffb343523aa
Rain Staub <rainstaub1@gmail.com>
2026-03-23 13:12:52 +0200

    refactor: a pet's status is now set to active on creation

--- src/main/java/com/petreg/prototype/dto/PetCreateDto.java
+++ src/main/java/com/petreg/prototype/dto/PetCreateDto.java
@@ -5,15 +5,12 @@ import java.time.LocalDate;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-import com.petreg.prototype.model.type.PetStatus;
-
 public record PetCreateDto(
 
         @NotBlank String name,
         @NotNull Character sex,
         @NotNull LocalDate birthDate,
         @NotBlank String color,
-        @NotNull PetStatus status,
         @NotNull Long breedId,
 
         Long microchipId,
--- src/main/java/com/petreg/prototype/dto/PetUpdateDto.java
+++ src/main/java/com/petreg/prototype/dto/PetUpdateDto.java
@@ -2,15 +2,12 @@ package com.petreg.prototype.dto;
 
 import java.time.LocalDate;
 
-import com.petreg.prototype.model.type.PetStatus;
-
 public record PetUpdateDto(
 
     String name,
     Character sex,
     LocalDate birthDate,
     String color,
-    PetStatus status,
     Long breedId,
     Long microchipId,
     Long ownerId,
