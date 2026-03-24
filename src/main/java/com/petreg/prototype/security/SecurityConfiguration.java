0dc6449bf13d7764aeaba1a6af5bec6b59d98a46
teadatuntud <265739897+teadatuntud@users.noreply.github.com>
2026-03-24 16:11:39 +0200

    feat: enable method security for RBAC

--- src/main/java/com/petreg/prototype/security/SecurityConfiguration.java
+++ src/main/java/com/petreg/prototype/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -21,6 +22,7 @@ import jakarta.servlet.DispatcherType;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     private RoleValidationFilter roleValidationFilter;
