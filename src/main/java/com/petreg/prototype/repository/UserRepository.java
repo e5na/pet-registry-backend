becb5e80e9ed2f92d499fedc28ef9334b17f24f1
teadatuntud <265739897+teadatuntud@users.noreply.github.com>
2026-03-24 13:31:27 +0200

    feat: allow to query users with roles without @Transactional set

--- src/main/java/com/petreg/prototype/repository/UserRepository.java
+++ src/main/java/com/petreg/prototype/repository/UserRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.petreg.prototype.model.User;
 import com.petreg.prototype.model.type.RoleEnum;
@@ -11,6 +13,9 @@ import com.petreg.prototype.model.type.RoleEnum;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByPersonalCode(String personalCode);
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.personalCode = :personalCode")
+    Optional<User> findByPersonalCodeWithRoles(@Param("personalCode") String personalCode);
 
     boolean existsByPersonalCode(String personalCode);
 
